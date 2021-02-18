a3574493b476b23e151509328da2500ff074c45b
Alfiia <ellie5768@gmail.com>
2021-02-17 19:22:41 -0800

    int commit

---
+++ JavaScript/script.js
@@ -0,0 +1,141 @@
+// STEP 1
+let someMonth;
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+/* function theMonth(); */
+
+// STEP 2
+
+ 10, 58 , 4.8 // Numeric Literal Expression
+ "Hello, World", 'Hello', " "            // String Literal Expression 
+ true , false // Boolean Literal Expression 
+ null // Null Literal Expression
+
+ // STEP 3
+ let compExpression = (5/7) * 7;
+ let varExp = (8 + 5) + ( 3 * 9);
+
+ // STEP 4 
+
+let firstName; 
+let lastName;
+let address;
+let city;
+let state;
+let zipCode;
+let yourAge;
+let referralSource;
+let mayWecontactYou; 
+
+// STEP 5 
+
+const PI = 3.14159; 
+city = "San Diego";
+let age = 28;
+
+
+// STEP 6
+
+let car = 2013 +  'Toyota';
+console.log(car);
+
+// STEP 7 
+let myStreet = false + "Street";
+console.log(myStreet);
+
+let price  = 1345 + true;
+console.log(price);
+
+//STEP 8  string" is a reserved keyword. For  a single quote we use  \' 
+ 
+let twoThings = 'Who once said,"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(twoThings);
+
+// STEP 9 
+
+let a = null;
+window.console.log(a);
+
+let yearsTeaching;
+window.console.log(yearsTeaching);
+
+// STEP 10 
+console.log(typeof "Toyota"); //  String
+
+console.log(typeof 2013 ); //  Number
+
+console.log(typeof false); // Boolean
+
+console.log(typeof undefined); // Undefined
+
+// STEP 11 
+
+a = "Hello Yury Osipov,";
+b = "welcome to the JavaScript class!";
+alert(a + " " + b); 
+
+// STEP 12 
+
+let namePerson = "Yury";
+alert(namePerson);
+
+// STEP 13 
+
+let ourClass = "JavaScript";
+alert(ourClass);
+
+// STEP 14
+
+let d = "Hello Yury Osipov.\nWelcome to the JavaScript class!";
+alert(d);
+
+//STEP 15
+
+let person = prompt("What is your name?" ," Yury Osipov");
+alert(person);
+
+// STEP 16
+
+let course = prompt("What class are you taking?" ,"JavaScript")
+alert(course);
+
+// STEP 17
+
+let x = 10;
+let y = 20;
+console.log(x + y)
+
+// STEP 18
+
+x = 20;
+console.log(x + 20);
+
+// STEP 19 
+ x = 20;
+ console.log(x * 5);
+
+ // STEP  20
+
+ x = 20 % 3;
+ console.log(x * 1);
+
+ // STEP 21
+ let t = 5;
+ let p = 3;
+ console.log(t > p); 
+
+ x = 10
+ y = 10
+ console.log(x == 10 && y == 10 );
+
+ // STEP 22 
+
+ f = 33;
+ p = 84;
+ console.log(f < p);
+
+ x = 14;
+ y = 56;
+ console.log( x !== 14 || y !== 56);
+
